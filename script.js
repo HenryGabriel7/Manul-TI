9dc9425dbc9e6264f678e935988ba8ed8bb04312
HenryGabriel7 <henrygabrielj6@gmail.com>
2026-03-10 11:43:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+function obterCaminhoLogin() {
+    const path = window.location.pathname;
+    const naRaiz = path.endsWith("index.html") || path.endsWith("/");
+    return naRaiz ? "login/login.html" : "../login/login.html";
+}
 
 (function verificarAcessoImediato() {
     if (localStorage.getItem("logado") !== "true") {
-        window.location.replace("login/login.html");
+        window.location.replace(obterCaminhoLogin());
     }
 })();
 
 function logout() {
     localStorage.clear();
-    window.location.replace("login/login.html");
+    window.location.replace(obterCaminhoLogin());
 }
 
 window.onpageshow = function(event) {
     if (event.persisted || localStorage.getItem("logado") !== "true") {
-        window.location.replace("login/login.html");
+        window.location.replace(obterCaminhoLogin());
     }
 };
